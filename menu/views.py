88af94c0c29bdd5bad96358d5f4da217d89d6b39
Ankush Khakale <khakaleankush@gmail.com>
2026-02-11 22:50:31 +0530

    Implemented views and templ

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, MenuItem
 
-# Create your views here.
+def menu_list(request):
+    categories = Category.objects.all()
+    # Prefetch related items to minimize queries
+    return render(request, 'menu/menu_list.html', {'categories': categories})
+
+def menu_detail(request, pk):
+    item = get_object_or_404(MenuItem, pk=pk)
+    return render(request, 'menu/menu_detail.html', {'item': item})
